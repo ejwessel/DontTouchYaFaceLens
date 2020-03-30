f02c1817526833311a5b607ba83f7dc367479fe2
Ethan Wessel <ejwessel@gmail.com>
2020-03-29 20:32:53 -0400

    Added instructions

--- Public/Face Overlap Detection.js
+++ Public/Face Overlap Detection.js
@@ -2,15 +2,19 @@
 // @input Component.ObjectTracking face
 // @input Component.ObjectTracking hand
 // @input Component.Text warning
+// @input Component.Text instructions
 
 // Plays the audio and hides the warning text
 
 var facePosition = script.face.getTransform().getLocalPosition()
 var handPosition = script.hand.getTransform().getLocalPosition()
 
-var textColor = script.warning.textFill.color
-var dropShadowColor = script.warning.dropshadowSettings.fill.color
-var outlineColor = script.warning.outlineSettings.fill.color
+var warningTextColor = script.warning.textFill.color
+var warningDropShadowColor = script.warning.dropshadowSettings.fill.color
+
+var instructionsTextColor = script.instructions.textFill.color
+var instructionsDropShadowColor = script.instructions.dropshadowSettings.fill.color
+
 
 print("Distance: " + handPosition.distance(facePosition))
 if (script.hand.isTracking() && handPosition.distance(facePosition) < 7) {
@@ -19,12 +23,17 @@ if (script.hand.isTracking() && handPosition.distance(facePosition) < 7) {
     originalColor.w = 1
     script.hand.getSceneObject().getChild(0).getComponent("Component.Image").getMaterial(0).mainPass.baseColor = originalColor
 
-    textColor.w = 1
-    dropShadowColor.w = 1
-    outlineColor.w = 1
-    script.warning.textFill.color = textColor
-    script.warning.dropshadowSettings.fill.color = dropShadowColor 
-    script.warning.outlineSettings.fill.color = outlineColor
+    // Show Warning
+    warningTextColor.w = 1
+    warningDropShadowColor.w = 1
+    script.warning.textFill.color = warningTextColor
+    script.warning.dropshadowSettings.fill.color = warningDropShadowColor
+    
+    // Hide Instructions
+    instructionsTextColor.w = 0
+    instructionsDropShadowColor.w = 0
+    script.instructions.textFill.color = instructionsTextColor
+    script.instructions.dropshadowSettings.fill.color = instructionsDropShadowColor
     
 } else {
     // FACE IS NOT BEING TOUCHED
@@ -32,10 +41,8 @@ if (script.hand.isTracking() && handPosition.distance(facePosition) < 7) {
     originalColor.w = 0
     script.hand.getSceneObject().getChild(0).getComponent("Component.Image").getMaterial(0).mainPass.baseColor = originalColor
     
-    textColor.w = 0
-    dropShadowColor.w = 0
-    outlineColor.w = 0
-    script.warning.textFill.color = textColor
-    script.warning.dropshadowSettings.fill.color = dropShadowColor 
-    script.warning.outlineSettings.fill.color = outlineColor    
+    warningTextColor.w = 0
+    warningDropShadowColor.w = 0
+    script.warning.textFill.color = warningTextColor
+    script.warning.dropshadowSettings.fill.color = warningDropShadowColor 
 }
